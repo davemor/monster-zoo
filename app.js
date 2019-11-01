3fa0473d8788e997b4b4164ac243b379251f2286
David Morrison <dave.mor@gmail.com>
2019-11-01 16:53:59 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// data model
+let nextMonsterId = 0;
+let monsters = makeMonsters(nextMonsterId, 3);
+let availableFood = 100;
+
+class Monster {
+    constructor(id) {
+        this.id = id;
+        this.name = Monster.makeName();
+        this.hunger = 0;
+    }
+    update() {
+        ++ this.hunger;
+    }
+    feed() {
+        this.hunger = 0;
+    }
+    static makeName() {
+        const starts = ['Idoap', 'Col', 'Mor', 'Jol'];
+        const middles = ['il', 'om', 'per', 'tek'];
+        const ends = ['osaurus', 'urid', 'odon', 'bit'];
+        const sample = (xs) => {
+            return xs[Math.floor(Math.random() * xs.length)];
+        }
+        return sample(starts) + sample(middles) + sample(ends);
+    }
+}
+
+function* range(start, end) {
+    for (let i = start; i <= end; i++) {
+        yield i;
+    }
+}
+
+function makeMonsters(startId, count) {
+    return range(startId, startId + count)
+           .map(id => new Monster(id));
+}
+
+app.get('/monsters', (req, res) => {
+    res.json(monsters); 
+});
+
+app.get('/monsters/:id', (req, res) => {
+    let index = req.params['id'];
+    if (index < monsters.length) {
+        let monster = monsters[index];
+        res.json(monster);
+    } else {
+        res.status(404).json('monster at index does not exist');
+    }
+});
+
+app.get('/food', (req, res) => {
+    res.json({availableFood:availableFood});
+});
+
+app.post('/food/:monsterId', (req, res) => {
+    let index = req.params['id'];
+    if (index < monsters.length) {
+        let monster = monsters[index];
+        monster.feed();
+        res.json(monster);
+    } else {
+        res.status(404).json('monster at index does not exist');
+    }
+});
+
+setInterval(function() {
+    console.log('Updating ...')
+    // update the monsters
+    for (let monster of monsters) {
+        monster.update();
+    }
+}, 5000);
+
+app.listen(port, () => console.log(`Monster Zoo running on port: ${port}!`));
